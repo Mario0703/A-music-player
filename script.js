0f77ed9df177152eb160b3a889dd119fc6f77c6e
Mario <mario@supermario.dk>
2023-02-23 18:02:51 +0100

    Added improvements to code

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
-const playBTN = document.getElementById("btn-play");
-const playBTN2 = document.getElementById("btn-play2");
-const playBTN3 = document.getElementById("btn-play3");
-const playBTN4 = document.getElementById("btn-play4");
-const playBTN5 = document.getElementById("btn-play5");
-const playBTN6 = document.getElementById("btn-play6");
-
-const audioFile = document.getElementById("player");
-const audioFile2 = document.getElementById("player2");
-const audioFile3 = document.getElementById("player3");
-const audioFile4 = document.getElementById("player4");
-const audioFile5 = document.getElementById("player5");
-const audioFile6 = document.getElementById("player6");
-
-const progressBar = document.getElementById("file");
-const progressBar2 = document.getElementById("file2");
-const progressBar3 = document.getElementById("file3");
-const progressBar4 = document.getElementById("file4");
-const progressBar6 = document.getElementById("file6");
-const progressBar5 = document.getElementById("file5");
+const playBTN = document.querySelectorAll("#btn-play")
+const audioFile = document.querySelectorAll("#player");
+const progressBar = document.querySelectorAll("#file");
 
 let paused = true;
 let Volume = document.getElementById("myRange") 
@@ -26,13 +9,9 @@ let currentVolume = Volume.value; // global variable to store current volume lev
 //Function that updates the audio changer
 function updateVolume() {
     currentVolume = Volume.value;
-    audioFile.volume = currentVolume;
-    audioFile2.volume = currentVolume;
-    audioFile3.volume = currentVolume;
-    audioFile4.volume = currentVolume;
-    audioFile5.volume = currentVolume;
-    audioFile6.volume = currentVolume;
-
+    audioFile.forEach((file) =>{
+        console.log(file);
+    });
 }
 
   
@@ -55,10 +34,9 @@ class Song{
         this.playbtn.onclick = () => {
             
             if(paused == true){
-            
                 this.file.play();
             
-                setInterval(() => {
+                requestAnimationFrame(() => {
                 const currentTime =  this.file.currentTime;
                 const duration =  this.file.duration;
                 const progress = (currentTime / duration) * 100;
@@ -85,18 +63,12 @@ class Song{
     }
 
 }
-let mySong  = new Song(audioFile,playBTN,progressBar,Volume);
-let mySong2  = new Song(audioFile2,playBTN2,progressBar2,Volume);
-let mySong3  = new Song(audioFile3,playBTN3,progressBar3,Volume);
-let mySong4  = new Song(audioFile4,playBTN4,progressBar4,Volume);
-let mySong5  = new Song(audioFile5,playBTN5,progressBar5,Volume);
-let mySong6  = new Song(audioFile6,playBTN6,progressBar6,Volume);
+const songs = [];
+for(let i = 0; i<audioFile.length; i++){
+    const song = new Song(audioFile[i],playBTN[i],progressBar[i],Volume);
+    songs.push(song);
+}
 
-
-
-mySong.play();
-mySong2.play();
-mySong3.play();
-mySong4.play();
-mySong5.play();
-mySong6.play();
+for(let i in songs){
+    songs[i].play();
+}
