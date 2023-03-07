f2df83e426f9d38651a1f0a5c2c61442dd95878d
Mario <42077299+Mario0703@users.noreply.github.com>
2023-03-07 22:18:11 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const playBTN = document.querySelectorAll("#btn-play")
+const playBTN = document.querySelectorAll("#btn-play");
 const audioFile = document.querySelectorAll("#player");
 const progressBar = document.querySelectorAll("#file");
 
@@ -8,7 +8,16 @@ let ShuffleINdex;
 let Volume = document.getElementById("myRange"); 
 let ResetBTN = document.getElementById("Reset-btn");
 let shuffle = document.getElementById("shuffle");
+let CurrentSong = document.getElementById("f");
+let Cover = document.getElementById("cover");
+let NextSongPlaylist = document.getElementById("next-song");
+let beforePlaylist = document.getElementById("song-before");
+
+
 let currentVolume = Volume.value; // global variable to store current volume level
+const songs_titles = ["See you again - Charlie Puth", "Dumb ways to die -Video game", "Infinity - LEMMiNO","Blinding Light -the Weeknd","Stor Mand - Tobias Rahim & Andreas Odbjerg", "Vi ta'r guldet med hjem - 8ball"];
+const song_images = ["SongCoverImages/See you again Cover.jpg","SongCoverImages/Dumb Ways to Die Cover.jpg","SongCoverImages/Lemmino Infinity Cover.jpg","SongCoverImages/The Weeknd Blinding Lights.jpg","SongCoverImages/Stor mand.jpg","SongCoverImages/8ball.jpg"]
+const SongMap = new Map();
 //Function that updates the audio changer
 function updateVolume() {
     currentVolume = Volume.value;
@@ -25,35 +34,38 @@ Creats a song class that wil represent all the song in the music player
 Here in the class it is possible to play pause and reset the songs.
 */
 class Song{
-    constructor(file, playbtn,ResetBTN,progressBAR,volumeSlider){
+    constructor(file, playbtn,ResetBTN,progressBAR,volumeSlider,artist,img){
         this.playbtn = playbtn; 
         this.file = file;
         this.progressBAR = progressBAR;
         this.volumeSlider = volumeSlider;
         this.ResetBTN = ResetBTN;
+        this.artist = artist;
+        this.img = img;
     }
 
     PLAY(){
         this.playbtn.onclick = () => {
             if(paused == true){
                 this.file.play();
-                isPLaying = this.file;
-                song.innerHTML = this.file.audioFile;
+                isPLaying = this.file;                
                 setInterval(() => {
                 const currentTime =  this.file.currentTime;
                 const duration =  this.file.duration;
                 const progress = (currentTime / duration) * 100;
-                this.progressBAR.value = progress
+                this.progressBAR.value = progress;
                 })
-
-            paused = false
-            this.playbtn.innerHTML = "||"
+            paused = false;
+            this.playbtn.innerHTML = "||";
+            CurrentSong.innerHTML = this.artist;
+            console.log(this.img)
+            Cover.src = this.img
             }
-       
+
             else{
                 this.playbtn.innerHTML = "▶"
                 this.progressBAR.value = 0;
-                paused = true
+                paused = true;
                 this.file.pause();
             }
 
@@ -74,13 +86,16 @@ class Song{
     }
 }
 
-shuffle.addEventListener("click",play)
+shuffle.addEventListener("click",play);
+NextSongPlaylist.addEventListener("click",next)
+beforePlaylist.addEventListener("click",next)
+
 const songs = [];
 const ShuffledQueue = [];
 
 //Push songs to array
 for(let i = 0; i<audioFile.length; i++){
-    const song = new Song(audioFile[i],playBTN[i],ResetBTN,progressBar[i],Volume);
+    const song = new Song(audioFile[i],playBTN[i],ResetBTN,progressBar[i],Volume,songs_titles[i],song_images[i]);
     songs.push(song);
 }
 
@@ -105,8 +120,7 @@ function play(){
         songs[index] = temp;
 
     }
-    console.log("Shuffled:",ShuffledQueue,"songs:",                                                                                         songs)
-    ShuffleINdex = 0
+    ShuffleINdex = 0;
     ShuffledQueue[0].shuffle();
 }
 //Play the next song in the queue
@@ -114,5 +128,3 @@ function next(){
     ShuffleINdex++;
     ShuffledQueue[ShuffleINdex].shuffle();
 }
-
-
